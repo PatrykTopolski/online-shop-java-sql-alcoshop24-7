7399a5a9a764c8dea7745be244ed3deb76a1be68
Stefania Hrycyk Kubat <stefania.hrycyk@gmail.com>
2019-04-18 13:51:24 +0200

    First version of controller

--- src/main/java/Controller/CustomerController.java
+++ src/main/java/Controller/CustomerController.java
@@ -2,8 +2,32 @@ package Controller;
 
 import java.sql.SQLException;
 import java.util.*;
+
+import Model.Order;
 import Model.Product;
 
 public class CustomerController{
+    CustomerDAOImpl dao = new CustomerDAOImpl();
+
+    public List<Product> getAllProducts() throws SQLException{
+        return dao.getAllProducts();
+    }
+
+    public List<Order> getOrders(int customerID)throws SQLException{
+        return dao.getOrders(customerID);
+    }
+
+    public Order getOrder(int orderID)throws SQLException{
+        return dao.getOrder(orderID);
+    }
+
+    public Product getProductById(int productID){
+        return dao.getProductById(productID);
+    }
+
+    public void makeNewOrder(int ID, int basketID, int userID){
+        dao.makeNewOrder(ID, basketID, userID);
+    }
+
 
 }
